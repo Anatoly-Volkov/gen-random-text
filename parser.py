64233822fd11cbbc85344c7aa737565acd23c2f8
Anatoly-Volkov <54665141+Anatoly-Volkov@users.noreply.github.com>
2019-09-03 23:45:13 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -2,22 +2,21 @@ import pickle
 import random
 import re
 
+
 class model:
     data = dict()
-    #key (first word, second word)
-    #value (array of possible words)
     words = []
-    #all words
-    
+    # key (first word, second word)
+    # value (array of possible words)
+
     def __init__(self, data, words):
         self.data = data
         self.words = words
-    
+
     def fit(self, file_name):
         f_r = open(file_name, "r")
         text = f_r.read()
         f_r.close()
-        
         text = text.lower()
         reg = re.compile("[^a-z'. ]")
         text = reg.sub('', text)
@@ -31,32 +30,28 @@ class model:
                 w1 = now[i]
                 w2 = now[i + 1]
                 w3 = now[i + 2]
-                if (self.data.get((w1, w2)) == None):
+                if (self.data.get((w1, w2)) is None):
                     self.data[(w1, w2)] = []
                 self.data[(w1, w2)].append(w3)
-    
-                
+
     def gen(self, L):
         arr = []
         while (L > 0):
             w1 = random.choice(self.words)
             w2 = random.choice(self.words)
-            while (self.data.get((w1, w2)) == None):
+            while (self.data.get((w1, w2)) is None):
                 w1 = random.choice(self.words)
                 w2 = random.choice(self.words)
             L -= 2
             print(w1[:1].upper() + w1[1:], end=' ')
             print(w2, end=' ')
-            while (L > 0 and self.data.get((w1, w2)) != None):
+            while (L > 0 and self.data.get((w1, w2)) is not None):
                 w3 = random.choice(self.data[(w1, w2)])
                 print(w3, end=' ')
-                #arr.append(w3)
                 L -= 1
                 w1, w2 = w2, w3
             print('.')
-        
- 
- 
+
 d1 = dict()
 w = []
 A = model(d1, w)
@@ -64,9 +59,9 @@ A = model(d1, w)
 with open('model_small.pickle', 'rb') as f:
     A = pickle.load(f)
 
-#A.fit("SW_EpisodeIV.txt")
-#A.fit("HP1.txt")
-#A.fit("wiki_small.txt")
+# A.fit("SW_EpisodeIV.txt")
+# A.fit("HP1.txt")
+# A.fit("wiki_small.txt")
 
 with open('model_small.pickle', 'wb') as f:
     pickle.dump(A, f)
